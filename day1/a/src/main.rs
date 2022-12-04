aedaa28fbb2ffd69f8755462610d5e86daaa52ea
kaapomoi <kaapomo@gmail.com>
2022-12-04 19:09:56 +0200

    Day one done.

--- day1/a/src/main.rs
+++ day1/a/src/main.rs
@@ -1,9 +1,19 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::ops::Index;
 use std::path::Path;
 
+fn insert_current_into_top_three(top_three: &mut [i32; 3], current: i32) {
+    let mut all_values = [top_three[0], top_three[1], current, top_three[2]];
+
+    all_values.sort();
+    all_values.reverse();
+
+    *top_three = [all_values[0], all_values[1], all_values[2]];
+}
+
 fn main() {
-    let mut largest = 0;
+    let mut top_three = [0; 3];
 
     let file = File::open(Path::new("input.txt")).expect("");
     let buffer = BufReader::new(file);
@@ -12,8 +22,8 @@ fn main() {
     let mut current = 0;
     for line in lines {
         if line == "" {
-            if largest < current {
-                largest = current;
+            if top_three[2] < current {
+                insert_current_into_top_three(&mut top_three, current);
             }
             current = 0;
         } else {
@@ -22,5 +32,7 @@ fn main() {
         }
     }
 
-    println!("{}", largest);
+    let mut total: i32 = top_three.iter().sum();
+
+    println!("{}", total);
 }
