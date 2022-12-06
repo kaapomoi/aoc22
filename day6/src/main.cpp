8cb1646ff56e64690a61bf7c0aaaabed5d0d3fc5
kaapomoi <kaapomo@gmail.com>
2022-12-06 12:29:16 +0200

    Part one done. 18 minutes

---
+++ day6/src/main.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+
+bool check_next(std::string const& s, int i, int n){
+    if (n == 0){
+        return true;
+    }
+
+    for (int j = i + 1; j < i + n; ++j) {
+        if (s[i] == s[j]){
+            return false;
+        }
+    }
+
+    return check_next(s, i+1, n-1);
+}
+
+int main()
+{
+    std::vector<std::string> lines;
+    std::uint64_t index{0};
+
+    std::ifstream infile("../input.txt");
+    std::string l;
+    while (std::getline(infile, l)) {
+        lines.push_back(l);
+    }
+
+    for (std::string const& line : lines) {
+        for (int i = 0; i < line.size(); ++i) {
+            bool success{check_next(line, i, 4)};
+
+            if (success){
+                index = i + 4;
+                break;
+            }
+        }
+    }
+
+    std::cout << index << "\n";
+    return 0;
+}
